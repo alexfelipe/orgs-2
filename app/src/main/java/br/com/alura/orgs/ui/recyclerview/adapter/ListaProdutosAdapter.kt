999e79f5c81dc80c868a9fb793d49693051a00ab
Alex Felipe <alexfelipevieira@gmail.com>
2021-06-08 14:44:23 -0300

    4.1

--- app/src/main/java/br/com/alura/orgs/ui/recyclerview/adapter/ListaProdutosAdapter.kt
+++ app/src/main/java/br/com/alura/orgs/ui/recyclerview/adapter/ListaProdutosAdapter.kt
@@ -7,7 +7,9 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import br.com.alura.orgs.R
+import br.com.alura.orgs.databinding.ProdutoItemBinding
 import br.com.alura.orgs.model.Produto
+import coil.load
 import java.text.NumberFormat
 import java.util.*
 import java.util.logging.SimpleFormatter
@@ -19,27 +21,28 @@ class ListaProdutosAdapter(
 
     private val produtos = produtos.toMutableList()
 
-    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+    class ViewHolder(private val binding: ProdutoItemBinding) : RecyclerView.ViewHolder(binding.root) {
 
         fun vincula(produto: Produto) {
-            val nome = itemView.findViewById<TextView>(R.id.produto_item_nome)
+            val nome = binding.produtoItemNome
             nome.text = produto.nome
-            val descricao = itemView.findViewById<TextView>(R.id.produto_item_descricao)
+            val descricao = binding.produtoItemDescricao
             descricao.text = produto.descricao
 
             val valorFormatado = NumberFormat
                 .getCurrencyInstance(Locale("pt", "br"))
                 .format(produto.valor)
-            val valor = itemView.findViewById<TextView>(R.id.produto_item_valor)
+            val valor = binding.produtoItemValor
             valor.text = valorFormatado
+            binding.produtoItemImageview.load("https://images.pexels.com/photos/2288683/pexels-photo-2288683.jpeg")
         }
 
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val inflater = LayoutInflater.from(context)
-        val view = inflater.inflate(R.layout.produto_item, parent, false)
-        return ViewHolder(view)
+        val binding = ProdutoItemBinding.inflate(inflater, parent, false)
+        return ViewHolder(binding)
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
